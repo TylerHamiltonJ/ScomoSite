5805d8239baafe40da499825853b4faa2b3c2439
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-08 17:30:05 +1000

    Updating time to be dynamic

--- src/logic/modules/days.js
+++ src/logic/modules/days.js
@@ -5,10 +5,10 @@ function updateTime() {
     const secondDate = new Date();
     const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
     const diffHoursRaw = Math.abs((firstDate - secondDate) / hours);
-    const diffHours = Math.abs(Math.round(diffHoursRaw - diffDays * 24))
-    const hoursLabel = diffHours === 1 ? 'hour' : 'hours'
-    const daysLabel = diffDays === 1 ? 'day' : 'days'
-    document.getElementById("timeSince").innerHTML = `<u>${diffDays} ${daysLabel} and ${diffHours} ${hoursLabel}</u>`;
+    const diffHours = Math.abs(Math.round(diffHoursRaw - diffDays * 24));
+    const hoursLabel = diffHours === 1 ? 'hour' : 'hours';
+    const daysLabel = diffDays === 1 ? 'day' : 'days';
+    document.getElementById('timeSince').innerHTML = `<u>${diffDays} ${daysLabel} and ${diffHours} ${hoursLabel}</u>`;
 }
 
 export { updateTime };
