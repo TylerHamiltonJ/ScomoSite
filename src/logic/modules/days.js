734da27485954da489f97a10427807f4f5a6a282
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-08 12:18:25 +1000

    init

--- src/logic/modules/days.js
+++ src/logic/modules/days.js
@@ -1,8 +1,8 @@
-import * as moment from "moment";
+import * as moment from 'moment';
 
 export const days = () => {
     var a = moment([2007, 0, 29]);
     var b = moment([2007, 0, 28]);
-    const days = a.diff(b, "days");
+    const days = a.diff(b, 'days');
     console.log(days);
 };
--- src/logic/root.js
+++ src/logic/root.js
@@ -5,15 +5,15 @@
 // DEPENDENCIES
 
 // modules
-import { smooth } from "modules/scroll";
+import { smooth } from 'modules/scroll';
 
 // components
-import header from "headers/1/_";
-import nav from "navigation/1/_";
-import footer from "footers/1/_";
+import header from 'headers/1/_';
+import nav from 'navigation/1/_';
+import footer from 'footers/1/_';
 
 // EXECUTION
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     nav();
     smooth();
     header();
