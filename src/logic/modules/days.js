e763fbd24a9257e69ee2dbf05a61da7f01b2db93
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-08 17:35:50 +1000

    Changing timezone

--- src/logic/modules/days.js
+++ src/logic/modules/days.js
@@ -1,11 +1,14 @@
 function updateTime() {
     const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
     const hours = 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-    const firstDate = new Date(2021, 6, 8, 3, 50);
+    const firstDate = new Date(2021, 6, 8, 13, 45);
     const secondDate = new Date();
-    const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+
+    console.log(firstDate)
+    console.log(secondDate)
+    const diffDays = Math.floor(Math.abs((firstDate - secondDate) / oneDay));
     const diffHoursRaw = Math.abs((firstDate - secondDate) / hours);
-    const diffHours = Math.abs(Math.round(diffHoursRaw - diffDays * 24));
+    const diffHours = Math.abs(Math.floor(diffHoursRaw - diffDays * 24));
     const hoursLabel = diffHours === 1 ? 'hour' : 'hours';
     const daysLabel = diffDays === 1 ? 'day' : 'days';
     document.getElementById('timeSince').innerHTML = `<u>${diffDays} ${daysLabel} and ${diffHours} ${hoursLabel}</u>`;
