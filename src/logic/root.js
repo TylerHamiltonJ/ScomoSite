a57bc48015897b7dfdf07bc919a41dbc76e884ff
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-12 13:35:51 +1000

    Removing blink

--- src/logic/root.js
+++ src/logic/root.js
@@ -5,22 +5,22 @@
 // DEPENDENCIES
 
 // modules
-import { smooth } from "modules/scroll";
-import { updateTime } from "modules/days";
+import { smooth } from 'modules/scroll';
+import { updateTime } from 'modules/days';
 
 // components
-import header from "headers/1/_";
-import nav from "navigation/1/_";
-import footer from "footers/1/_";
+import header from 'headers/1/_';
+import nav from 'navigation/1/_';
+import footer from 'footers/1/_';
 
 // EXECUTION
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     nav();
     smooth();
     header();
     footer();
     updateTime();
-    setInterval(function () {
+    setInterval(function() {
         updateTime();
     }, 60000);
 });
