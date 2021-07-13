9c87249d3cd52ca3bd9b2096400ea7842d3533e6
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-13 15:33:28 +1000

    Fixing recent date

--- src/logic/modules/days.js
+++ src/logic/modules/days.js
@@ -1,11 +1,11 @@
 const bodyCopy = `<b>Update! Scott Morrison has recently been spotted doing a softball interview on SkyNews.</b>
 <br/>A very big thank you to everyone who worked tirelessly to find him.`;
 
-const schedule = [new Date(2021, 6, 12, 17, 0), new Date(2021, 6, 13, 17, 30)];
+const schedule = [new Date(2021, 6, 12, 17, 0), new Date(2021, 6, 13, 15, 30)];
 
 const getMostRecentConference = () => {
     const sorted = schedule
-        .sort(function(a, b) {
+        .sort(function (a, b) {
             return b - a;
         })
         .filter((f) => {
@@ -22,17 +22,17 @@ function updateTime() {
     const diffDays = Math.floor(Math.abs((firstDate - secondDate) / oneDay));
     const diffHoursRaw = Math.abs((firstDate - secondDate) / hours);
     const diffHours = Math.abs(Math.floor(diffHoursRaw - diffDays * 24));
-    const hoursLabel = diffHours === 1 ? 'hour' : 'hours';
-    const daysLabel = diffDays === 1 ? 'day' : 'days';
+    const hoursLabel = diffHours === 1 ? "hour" : "hours";
+    const daysLabel = diffDays === 1 ? "day" : "days";
     document.getElementById(
-        'timeSince'
+        "timeSince"
     ).innerHTML = `<u>${diffDays} ${daysLabel} and ${diffHours} ${hoursLabel}</u>`;
-    const bodyCopyDOM = document.getElementById('bodyCopy');
+    const bodyCopyDOM = document.getElementById("bodyCopy");
     if (diffHoursRaw <= 24) {
         bodyCopyDOM.innerHTML = bodyCopy;
-        bodyCopyDOM.style.display = 'visible';
+        bodyCopyDOM.style.display = "visible";
     } else {
-        bodyCopyDOM.style.display = 'none';
+        bodyCopyDOM.style.display = "none";
     }
 }
 
