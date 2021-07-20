32a0a9f7180fddd55952454b315298bcac4e8e79
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-20 15:20:02 +1000

    Randomising tagline

--- src/logic/modules/tagline.js
+++ src/logic/modules/tagline.js
@@ -1,14 +1,14 @@
 const taglines = [
-    `That's about how long it takes for <a href="https://www.abc.net.au/news/2021-07-11/kevin-rudd-australia-covid-pfizer-vaccine-supply-senior-execs/100284902">a former PM to negotiate a new vaccine rollout.</a>`,
-    `That's about how long it takes to <a href="https://www.theguardian.com/australia-news/2021/mar/23/coalition-staffer-sacked-for-alleged-sex-act-on-female-mps-desk-was-longtime-liberal-aide">wipe down a parliamentary desk.`,
-    `That's about how long it takes to <a href="https://www.smh.com.au/national/nsw/year-12-students-at-st-joseph-s-college-given-pfizer-vaccination-20210706-p587bd.html">Vaccinate a cohort of private school boys.</a>`,
+    'That\'s about how long it takes for <a href="https://www.abc.net.au/news/2021-07-11/kevin-rudd-australia-covid-pfizer-vaccine-supply-senior-execs/100284902">a former PM to negotiate a new vaccine rollout.</a>',
+    'That\'s about how long it takes to <a href="https://www.theguardian.com/australia-news/2021/mar/23/coalition-staffer-sacked-for-alleged-sex-act-on-female-mps-desk-was-longtime-liberal-aide">wipe down a parliamentary desk.',
+    'That\'s about how long it takes to <a href="https://www.smh.com.au/national/nsw/year-12-students-at-st-joseph-s-college-given-pfizer-vaccination-20210706-p587bd.html">Vaccinate a cohort of private school boys.</a>',
 ];
 
 const ranArr = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 function updateTagline() {
-    const taglineDOM = document.getElementById("tagLine");
-    const tagline = ranArr(taglines)
+    const taglineDOM = document.getElementById('tagLine');
+    const tagline = ranArr(taglines);
     taglineDOM.innerHTML = tagline;
 }
 
--- src/logic/root.js
+++ src/logic/root.js
@@ -5,24 +5,24 @@
 // DEPENDENCIES
 
 // modules
-import { smooth } from "modules/scroll";
-import { updateTime } from "modules/days";
-import { updateTagline } from "modules/tagline";
+import { smooth } from 'modules/scroll';
+import { updateTime } from 'modules/days';
+import { updateTagline } from 'modules/tagline';
 
 // components
-import header from "headers/1/_";
-import nav from "navigation/1/_";
-import footer from "footers/1/_";
+import header from 'headers/1/_';
+import nav from 'navigation/1/_';
+import footer from 'footers/1/_';
 
 // EXECUTION
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     nav();
     smooth();
     header();
     footer();
     updateTagline();
     updateTime();
-    setInterval(function () {
+    setInterval(function() {
         updateTime();
     }, 60000);
 });
