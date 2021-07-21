14aabb885474cf93288e149220445fdc991804b7
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-21 10:56:53 +1000

    Removing console log

--- src/logic/modules/tagline.js
+++ src/logic/modules/tagline.js
@@ -10,7 +10,7 @@ const taglines = [
 const ranArr = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 function updateTagline() {
-    console.log("Oi! Get outta my code.")
+    console.log('Oi! Get outta my code.');
     const taglineDOM = document.getElementById('tagLine');
     const tagline = ranArr(taglines);
     taglineDOM.innerHTML = tagline;
