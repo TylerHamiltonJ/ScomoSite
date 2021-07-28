39fe377febdb74e4547d20d37959a0a7d0ab62e1
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-28 13:27:05 +1000

    Updating last seen time

--- src/logic/modules/days.js
+++ src/logic/modules/days.js
@@ -1,7 +1,7 @@
 const bodyCopy =
     '<b>Update! Scott Morrison was most recently spotted in Canberra, denying responsibility but taking credit.</b>';
 
-const schedule = [new Date(2021, 6, 12, 17, 0), new Date(2021, 6, 25, 13, 15)];
+const schedule = [new Date(2021, 6, 12, 17, 0), new Date(2021, 6, 28, 13, 15)];
 
 const sighting = new Date(2021, 6, 21, 8, 0);
 
